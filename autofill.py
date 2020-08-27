8e005954751af7f4824623d8cf5c4f76e5fc8591
SinghHimadri <himsingh1612@gmail.com>
2020-08-27 22:05:21 +0530

    test

---
+++ autofill.py
@@ -0,0 +1,194 @@
+# egion IMPORTS
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+import os
+import time
+import job_list
+# region END
+
+
+# Fill your personal details
+INFORMATION = {
+    "first_name": "Foo",
+    "last_name": "Bar",
+    "email": "eg@eg.com",
+    "phone": "1234567",
+    "org": "Self-Employed",
+    "resume": "resume.pdf",
+    "resume_textfile": "resume_short.txt",
+    "linkedin": "https://www.linkedin.com/",
+    "website": "www.youtube.com",
+    "github": "https://github.com",
+    "twitter": "www.twitter.com",
+    "location": "ABC, XYZ",
+    "grad_month": '06',
+    "grad_year": '2021',
+    "university": "IIIT"
+}
+
+
+# Parsing Lever Application form
+def lever(driver):
+
+    # Open the application web page
+    driver.find_element_by_class_name('template-btn-submit').click()
+
+    # Fill Personals
+    first_name = INFORMATION['first_name']
+    last_name = INFORMATION['last_name']
+    full_name = first_name + ' ' + last_name
+    driver.find_element_by_name('name').send_keys(full_name)
+    driver.find_element_by_name('email').send_keys(INFORMATION['email'])
+    driver.find_element_by_name('phone').send_keys(INFORMATION['phone'])
+    driver.find_element_by_name('org').send_keys(INFORMATION['org'])
+
+    # socials
+    driver.find_element_by_name('urls[LinkedIn]').send_keys(INFORMATION['linkedin'])
+    driver.find_element_by_name('urls[Twitter]').send_keys(INFORMATION['twitter'])
+
+    try:
+        driver.find_element_by_name('urls[Github]').send_keys(INFORMATION['github'])
+    except NoSuchElementException:
+        try:
+            driver.find_element_by_name('urls[GitHub]').send_keys(INFORMATION['github'])
+        except NoSuchElementException:
+            pass
+    driver.find_element_by_name('urls[Portfolio]').send_keys(INFORMATION['website'])
+
+    # University(if asked)
+    try:
+        driver.find_element_by_class_name('application-university').click()
+        search = driver.find_element_by_xpath("//*[@type='search']")
+        search.send_keys(INFORMATION['university'])  # find university in dropdown
+        search.send_keys(Keys.RETURN)
+    except NoSuchElementException:
+        pass
+
+    # add how you found out about the company
+    try:
+        driver.find_element_by_class_name('application-dropdown').click()
+        search = driver.find_element_by_xpath("//select/option[text()='Glassdoor']").click()
+    except NoSuchElementException:
+        pass
+
+    # Resume clickable file-upload
+    driver.find_element_by_name('resume').send_keys(os.getcwd()+"/resume.pdf")
+    driver.find_element_by_class_name('template-btn-submit').click()
+
+
+# Parsing Greenhouse application form
+def greenhouse(driver):
+
+    # basic info
+    driver.find_element_by_id('first_name').send_keys(INFORMATION['first_name'])
+    driver.find_element_by_id('last_name').send_keys(INFORMATION['last_name'])
+    driver.find_element_by_id('email').send_keys(INFORMATION['email'])
+    driver.find_element_by_id('phone').send_keys(INFORMATION['phone'])
+
+    # If doesn't works user has to complete the action
+    try:
+        loc = driver.find_element_by_id('job_application_location')
+        loc.send_keys(INFORMATION['location'])
+        loc.send_keys(Keys.DOWN)  # manipulate a dropdown menu
+        loc.send_keys(Keys.DOWN)
+        loc.send_keys(Keys.RETURN)
+        time.sleep(2)  # give user time to manually input if this fails
+
+    except NoSuchElementException:
+        pass
+
+    # Upload Resume as a Text File in Resume paste option
+    driver.find_element_by_css_selector("[data-source='paste']").click()
+    resume_zone = driver.find_element_by_id('resume_text')
+    resume_zone.click()
+    with open(INFORMATION['resume_textfile']) as f:
+        lines = f.readlines()  # add each line of resume to the text area
+        for line in lines:
+            resume_zone.send_keys(line.decode('utf-8'))
+
+    # linkedin
+    try:
+        driver.find_element_by_xpath("//label[contains(.,'LinkedIn')]").send_keys(INFORMATION['linkedin'])
+    except NoSuchElementException:
+        try:
+            driver.find_element_by_xpath("//label[contains(.,'Linkedin')]").send_keys(INFORMATION['linkedin'])
+        except NoSuchElementException:
+            pass
+# User to make changes here for drop down menu
+    # graduation year
+    try:
+        driver.find_element_by_xpath("//select/option[text()='2021']").click()
+    except NoSuchElementException:
+        pass
+
+    # university
+    try:
+        driver.find_element_by_xpath("//select/option[contains(.,'Harvard')]").click()
+    except NoSuchElementException:
+        pass
+
+    # degree
+    try:
+        driver.find_element_by_xpath("//select/option[contains(.,'Bachelor')]").click()
+    except NoSuchElementException:
+        pass
+
+    # major
+    try:
+        driver.find_element_by_xpath("//select/option[contains(.,'Computer Science')]").click()
+    except NoSuchElementException:
+        pass
+
+    # website
+    try:
+        driver.find_element_by_xpath("//label[contains(.,'Website')]").send_keys(INFORMATION['website'])
+    except NoSuchElementException:
+        pass
+
+    # work authorization
+    try:
+        driver.find_element_by_xpath("//select/option[contains(.,'any employer')]").click()
+    except NoSuchElementException:
+        pass
+
+    driver.find_element_by_id("submit_app").click()
+
+
+# main
+if __name__ == '__main__':
+
+    # scrape job listings from glassdoor
+    aggregatedURLs = job_list.getURLs()
+    print(f'Job Listings: {aggregatedURLs}')
+    print('\n')
+
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    for url in aggregatedURLs:
+        print('\n')
+
+        if 'greenhouse' in url:
+            driver.get(url)
+            try:
+                greenhouse(driver)
+                print(f'SUCCESS FOR: {url}')
+            except Exception:
+                # print(f"FAILED FOR {url}")
+                continue
+
+        elif 'lever' in url:
+            driver.get(url)
+            try:
+                lever(driver)
+                print(f'SUCCESS FOR: {url}')
+            except Exception:
+                # print(f"FAILED FOR {url}")
+                continue
+        else:
+            # print(f"NOT A VALID APP LINK FOR {url}")
+            continue
+
+        time.sleep(2)
+
+    driver.close()
---
+++ job_list.py
@@ -0,0 +1,182 @@
+# region IMPORTS
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+from bs4 import BeautifulSoup
+import urllib.request
+import re
+# region END
+
+
+# dictionary of user's preferenes
+# make changes here
+CHOICES = {
+    "job_title": "Android Developer",
+    "location": "Bengaluru (India)"
+}
+
+
+# opens glassdoor website and waits until user login
+def glassdoor_config(driver):
+
+    driver.get('https://www.glassdoor.com/index.htm')
+
+    # keep waiting for user to log-in until the URL changes to user page
+    while True:
+        try:
+            WebDriverWait(driver, 1).until(EC.url_contains("member"))
+        except TimeoutException:
+            break
+    return True
+
+
+# fill entries and search jobs
+def search_job(driver):
+
+    # wait until search bar to appears
+    element = WebDriverWait(driver, 20).until(
+            EC.presence_of_element_located((By.XPATH, "//*[@id='scBar']"))
+        )
+
+    try:
+        # locating company and location bar
+        company_field = driver.find_element_by_xpath("//*[@id='sc.keyword']")
+        location_field = driver.find_element_by_xpath("//*[@id='sc.location']")
+        company_field.clear()
+        location_field.clear()
+
+        # filling user's choice
+        company_field.clear()
+        company_field.send_keys(CHOICES['job_title'])
+        location_field.clear()
+        location_field.send_keys(CHOICES['location'])
+
+        # clicking search bar
+        time.sleep(1)
+        driver.find_element_by_xpath(" //*[@id='scBar']/div/button").click()
+
+        # close a random popup if it shows up
+        try:
+            driver.find_element_by_xpath("//*[@id='JAModal']/div/div[2]/span").click()
+        except NoSuchElementException:
+            pass
+
+        return True
+
+    except NoSuchElementException:
+        return False
+
+
+# get all url links in a set
+def get_links(driver):
+
+    allLinks = []  # all hrefs list that exist on the page
+    # wait for page to fully load
+    element = WebDriverWait(driver, 20).until(
+            EC.presence_of_element_located((By.XPATH, "//*[@id='MainCol']/div[1]/ul"))
+        )
+    time.sleep(5)
+
+    # parsing the page source using beautiful soup
+    page_source = driver.page_source
+    soup = BeautifulSoup(page_source)
+
+    # find all hrefs with class tag job
+    allJobLinks = soup.findAll("a", {"class": "jobLink"})
+    allLinks = [jobLink['href'] for jobLink in allJobLinks]
+    allFixedLinks = []
+
+    # clean up the job links by opening, modifying, and 'unraveling' the URL
+    for link in allLinks:
+        # first, replace GD_JOB_AD with GD_JOB_VIEW
+        # this will replace the Glassdoor hosted job page to the proper job page
+        link = link.replace("GD_JOB_AD", "GD_JOB_VIEW")
+
+        # if there is no glassdoor prefex, add that
+        # for example, /partner/jobListing.htm?pos=121... needs the prefix
+
+        if link[0] == '/':
+            link = f"https://www.glassdoor.com{link}"
+        # then, open up each url and save the result url
+        # because we got a 403 error when opening this normally, we have to establish the user agent
+        user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
+        headers = {'User-Agent': user_agent, }
+        request = urllib.request.Request(link, None, headers)  # The assembled request
+
+        try:
+            # the url is on glassdoor itself, but once it's opened, it redirects - so let's store that the job page
+            response = urllib.request.urlopen(request)
+            newLink = response.geturl()
+
+            # if the result url is from glassdoor, it's an 'easy apply' one and worth not saving
+            if "glassdoor" not in newLink:
+                print(newLink)
+                print('\n')
+                allFixedLinks.append(newLink)
+        except Exception:
+            print(f'ERROR: failed for {link}')
+            print('\n')
+
+    # convert to a set to eliminate duplicates
+    return set(allFixedLinks)
+
+
+# method to iterate through all pages and aggregate URLs
+def getURLs():
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    success = glassdoor_config(driver)
+    if not success:
+        # close the page if it gets stuck at some point - this logic can be improved
+        driver.close()
+
+    success = search_job(driver)
+    if not success:
+        driver.close()
+
+    allLinks = set()
+    page = 1
+    next_url = ''
+    while page < 5:  # User enter the number of pages so this doesn't run infinitely
+
+        print(f'\nNEXT PAGE #: {page}\n')
+
+        # on the first page, the URL is unique and doesn't have a field for the page number
+        if page == 1:
+
+            # aggregate links on first page
+            allLinks.update(get_links(driver))
+
+            # find next page button and click it
+            next_page = driver.find_element_by_xpath("//*[@id='FooterPageNav']/div/ul/li[3]/a")
+            this_page = next_page.get_attribute('href')
+
+            # use regex to parse out the page number
+            m = re.search('(?P<url>[^;]*?)(?P<page>.htm\?p=)(?P<pagenum>.)', this_page)
+
+            # for page 2 onwards, there's a different page structure that we need to convert from
+            # from: .../jobs-SRCH_IL.0,13_IC1147401_KE14,33.htm?p=2
+            # to: .../jobs-SRCH_IL.0,13_IC1147401_KE14,33_IP2.htm
+            page += 1  # increment page count
+            next_url = f"{m.group('url')}_IP{page}.htm"  # update url with new page number
+            time.sleep(1)
+
+        # same patterns from page 2 onwards
+        if page >= 2:
+
+            driver.get(next_url)  # open page with new URL
+            allLinks.update(get_links(driver))  # collect all the links
+
+            # run regex to get all reusable parts of URL
+            m = re.search('(?P<url>[^;]*?)(?P<pagenum>.)(?P<html>.htm)', next_url)
+            page += 1
+            next_url = f"{m.group('url')}{page}.htm"
+
+    driver.close()
+    return allLinks
+# run the below commands to get the list(and don't want to apply)
+# if __name__ == '__main__':
+#     getURLs()
